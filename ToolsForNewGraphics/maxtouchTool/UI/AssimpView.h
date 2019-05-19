40def32366efe8c6e03823f574ff184a2f447dad
Katekov Anton <void0main@gmail.com>
2019-05-19 12:02:34 +0900

    Importer UI

---
+++ ToolsForNewGraphics/maxtouchTool/UI/AssimpView.h
@@ -0,0 +1,20 @@
+//
+//  AssimpView.h
+//  maxtouchTool
+//
+//  Created by Katekov Anton on 5/19/19.
+//  Copyright © 2019 katekovanton. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <AppKit/AppKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AssimpView : NSView {
+    IBOutlet NSOutlineView *_table;
+}
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ToolsForNewGraphics/maxtouchTool/UI/AssimpView.m
@@ -0,0 +1,21 @@
+//
+//  AssimpView.m
+//  maxtouchTool
+//
+//  Created by Katekov Anton on 5/19/19.
+//  Copyright © 2019 katekovanton. All rights reserved.
+//
+
+#import "AssimpView.h"
+
+
+
+@implementation AssimpView
+
+- (IBAction)onOpen:(id)sender
+{}
+
+- (IBAction)onExport:(id)sender
+{}
+
+@end
