db15f3dd868ae9239e19aa66ae66a8391689a9f5
KatekovAnton <void0main@gmail.com>
2024-04-26 00:39:11 +0900

    Shorten the example

--- ToolsForNewGraphics/maxtouchTool/main.cpp
+++ ToolsForNewGraphics/maxtouchTool/main.cpp
@@ -4,20 +4,13 @@
 #include <BinaryPack.hpp>
 #include <BinaryPackUtilities.hpp>
 #include <MAXContentMap.h>
+#include <MAXContentUtils.h>
 
 int main() {
     std::cout << "Hello tools" << std::endl;;
 
-    std::ifstream file("Resources/1559135259.map", std::ifstream::binary);
-    file.seekg(0, std::ifstream::end);
-    size_t fsize = file.tellg();
-    file.seekg(0, std::ifstream::beg);
-
-    ByteBuffer b(fsize);
-    b.increaseBuffer();
-    file.read(reinterpret_cast<char *>(b.getPointer()), fsize);
-    b.dataAppended(fsize);
-    file.close();
+    ByteBuffer b;
+    MAXContentUtils::ReadFileToBuffer("Resources/1559135259.map", &b);
 
     BinaryPack p;
     BinaryPackReader pr(&p, &b);
