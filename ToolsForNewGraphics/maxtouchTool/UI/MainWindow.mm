398f3059b9586486a70431f46c4f94edebbac9c4
Anton Katekov <void0main@gmail.com>
2025-09-20 01:52:56 +0900

    Longer wait to let file window animation to finish

--- ToolsForNewGraphics/maxtouchTool/UI/MainWindow.mm
+++ ToolsForNewGraphics/maxtouchTool/UI/MainWindow.mm
@@ -122,7 +122,7 @@
 
 - (void)processDirectories:(NSArray*)directories toOutputDirectory:(NSString*)outputDirectory options:(MTOptions)options
 {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         for (NSString *path in directories) {
             MTVisualObjectController *controller = [[MTVisualObjectController alloc] initWithInputPath:path outputPath:outputDirectory];
             [controller dowork:options];
